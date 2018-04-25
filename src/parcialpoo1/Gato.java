078567d3b8db38328772184cd475aea397fa8e9f
nguardadov <nguardadov@gmail.com>
2018-04-25 15:34:04 -0600

    Se agrego la clase Gato

---
+++ src/parcialpoo1/Gato.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialpoo1;
+
+/**
+ *
+ * @author nelso
+ */
+public class Gato {
+    private int edad;
+
+    public Gato() {
+    }
+
+    public Gato(int edad) {
+        this.edad = edad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+}
